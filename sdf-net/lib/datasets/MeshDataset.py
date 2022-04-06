f61d85a6e9aecf06f0539ff5761b6d447d629833
Liraz-Benbenishti <Lirazben6@gmail.com>
2022-04-06 08:06:48 +0300

    Alternative SDF Calculation using kD-Tree.

--- sdf-net/lib/datasets/MeshDataset.py
+++ sdf-net/lib/datasets/MeshDataset.py
@@ -26,11 +26,122 @@ from torch.utils.data import Dataset
 
 import numpy as np
 import mesh2sdf
+import laspy
+from scipy import spatial
 
-from lib.torchgp import load_obj, point_sample, sample_surface, compute_sdf, normalize
+from lib.torchgp import load_obj, point_sample, sample_surface, compute_sdf, normalize, load_las
 from lib.PsDebugger import PsDebugger
 
 from lib.utils import PerfTimer, setparam
+
+import collections
+import operator
+from datetime import datetime
+
+BT = collections.namedtuple("BT", ["value", "left", "right"])
+BT.__doc__ = """
+A Binary Tree (BT) with a node value, and left- and
+right-subtrees.
+"""
+
+def kdtree(points):
+    """Construct a k-d tree from an iterable of points.
+    
+    This algorithm is taken from Wikipedia. For more details,
+    
+    > https://en.wikipedia.org/wiki/K-d_tree#Construction
+    
+    """
+    k = len(points[0])
+    
+    def build(*, points, depth):
+        """Build a k-d tree from a set of points at a given
+        depth.
+        """
+        if len(points) == 0:
+            return None
+        
+        points.sort(key=operator.itemgetter(depth % k))
+        middle = len(points) // 2
+        
+        return BT(
+            value = points[middle],
+            left = build(
+                points=points[:middle],
+                depth=depth+1,
+            ),
+            right = build(
+                points=points[middle+1:],
+                depth=depth+1,
+            ),
+        )
+    
+    return build(points=list(points), depth=0)
+
+NNRecord = collections.namedtuple("NNRecord", ["point", "distance"])
+NNRecord.__doc__ = """
+Used to keep track of the current best guess during a nearest
+neighbor search.
+"""
+
+def find_nearest_neighbor(*, tree, point):
+    """Find the nearest neighbor in a k-d tree for a given
+    point.
+    """
+    k = len(point)
+    
+    best = None
+    def search(*, tree, depth):
+        """Recursively search through the k-d tree to find the
+        nearest neighbor.
+        """
+        nonlocal best
+        
+        if tree is None:
+            return
+        
+        distance = SED(tree.value, point)
+        if best is None or distance < best.distance:
+            best = NNRecord(point=tree.value, distance=distance)
+        
+        axis = depth % k
+        diff = point[axis] - tree.value[axis]
+        if diff <= 0:
+            close, away = tree.left, tree.right
+        else:
+            close, away = tree.right, tree.left
+        
+        search(tree=close, depth=depth+1)
+        if diff**2 < best.distance:
+            search(tree=away, depth=depth+1)
+    
+    search(tree=tree, depth=0)
+    return best.point
+
+def nearest_neighbor_kdtree(*, query_points, reference_points):
+    """Use a k-d tree to solve the "Nearest Neighbor Problem"."""
+    tree = kdtree(reference_points)
+    print("Finished creating kdtree: ", datetime.now())
+    return {
+        query_p: find_nearest_neighbor(tree=tree, point=query_p)
+        for query_p in query_points
+    }
+
+def SED(X, Y):
+    """Compute the squared Euclidean distance between X and Y."""
+    return np.sqrt(sum((i-j)**2 for i, j in zip(X, Y)))
+
+def nearest_neighbor_bf(*, query_points, reference_points):
+    """Use a brute force algorithm to solve the
+    "Nearest Neighbor Problem".
+    """
+    return [
+        SED(query_p, min(
+            reference_points,
+            key=lambda X: SED(X, query_p),
+        ))
+        for query_p in query_points
+    ]
 
 class MeshDataset(Dataset):
     """Base class for single mesh datasets."""
@@ -66,10 +177,9 @@ class MeshDataset(Dataset):
             out = load_obj(self.dataset_path, load_materials=True)
             self.V, self.F, self.texv, self.texf, self.mats = out
         else:
-            self.V, self.F = load_obj(self.dataset_path)
-
-        self.V, self.F = normalize(self.V, self.F)
-        self.mesh = self.V[self.F]
+            self.V, self.F = load_obj(self.dataset_path) # load_obj / las
+        self.V = normalize(self.V)
+        #self.mesh = self.V[self.F]
         self.resample()
 
     def resample(self):
@@ -82,7 +192,31 @@ class MeshDataset(Dataset):
         else:
             self.pts = point_sample(self.V, self.F, self.sample_mode, self.num_samples)
 
-        self.d = compute_sdf(self.V.cuda(), self.F.cuda(), self.pts.cuda())   
+        #self.d = compute_sdf(self.V.cuda(), self.F.cuda(), self.pts.cuda())   
+        
+        #reference_points = [ (1, 2), (3, 2), (4, 1), (3, 5) ]
+        #query_points = [
+        #    (3, 4), (5, 1), (7, 3), (8, 9), (10, 1), (3, 3)
+        #]
+
+        print("Started: ", datetime.now())
+        #nearest_neighbor_bf(
+        #    reference_points = self.V,
+        #    query_points = self.pts,
+        #)
+
+        #tree = kdtree(self.V)
+        #find_nearest_neighbor(tree=tree, point=(10, 1))
+        self.d = nearest_neighbor_kdtree(
+            reference_points = self.V,
+            query_points = self.pts,
+        )
+        print("Finished: ", datetime.now())
+
+        #tree = spatial.KDTree(self.V)
+        #self.d = [list[1] for list in tree.query(self.V, k = 2)[0]]
+        self.d = torch.as_tensor(self.d)
+        print("self d: ", self.d.size())
 
         self.d = self.d[...,None]
         self.d = self.d.cpu()
--- sdf-net/lib/torchgp/normalize.py
+++ sdf-net/lib/torchgp/normalize.py
@@ -22,17 +22,15 @@
 import torch
 
 def normalize(
-    V : torch.Tensor,
-    F : torch.Tensor):
+    V : torch.Tensor):
 
     # Normalize mesh
     V_max, _ = torch.max(V, dim=0)
     V_min, _ = torch.min(V, dim=0)
     V_center = (V_max + V_min) / 2.
     V = V - V_center
-
     # Find the max distance to origin
     max_dist = torch.sqrt(torch.max(torch.sum(V**2, dim=-1)))
-    V_scale = 1. / max_dist
+    V_scale = 1. / 30#max_dist
     V *= V_scale
-    return V, F
+    return V
